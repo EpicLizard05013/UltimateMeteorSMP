4b028d4d698f8e92c81dfd41e05dd0889d84b261
EpicLizard05013 <bailinwu610@gmail.com>
2026-09-25 20:01:19 -0400

    Fix build.gradle.kts syntax and Vault version

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
-    compileOnly("com.github.MilkBowl:VaultAPI:2.0") {
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
         exclude(group = "org.bukkit", module = "bukkit")
     }
     compileOnly("com.github.SkriptLang:Skript:2.16.2")
